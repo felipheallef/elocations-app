afff48ec223a3a633384b0bad8ee8ab3b72eb1ef
felipheallef <felipheallef@gmail.com>
2021-05-17 13:26:48 -0300

    Project finished.
    Challenge project for the Kotlin Developer job at Engeselt completed.

--- app/src/main/java/com/felipheallef/elocations/Application.kt
+++ app/src/main/java/com/felipheallef/elocations/Application.kt
@@ -2,8 +2,11 @@ package com.felipheallef.elocations
 
 import android.app.Application
 import android.content.Context
+import androidx.core.graphics.toColor
 import androidx.room.Room
 import com.felipheallef.elocations.data.AppDatabase
+import com.felipheallef.elocations.data.model.Business
+import com.google.gson.Gson
 
 class Application : Application() {
 
@@ -12,6 +15,24 @@ class Application : Application() {
 
         database = Room.databaseBuilder(this, AppDatabase::class.java, "database")
             .allowMainThreadQueries().build()
+
+        val prefs = getSharedPreferences(getString(R.string.pref_name), Context.MODE_PRIVATE)
+        val firstRun = prefs.getBoolean("first_run", true)
+
+        if (firstRun) {
+            val business = Business("Engeselt Engenharia e Serviços",
+                "A ENGESELT oferece serviços com foco no alto padrão de qualidade para garantir aos seus clientes uma parceria de sucesso.",
+                "(83) 3268-5743", "Empresa",
+                -7.1697895, -34.8632032)
+
+            val inserted = database?.businessDao()?.insert(business)
+
+            if (inserted != 0.toLong()) {
+                val prefsEditor = prefs.edit()
+                prefsEditor.putBoolean("first_run", false)
+                prefsEditor.apply()
+            }
+        }
 
         //Stetho
 //        val initializerBuilder = Stetho.newInitializerBuilder(this)
